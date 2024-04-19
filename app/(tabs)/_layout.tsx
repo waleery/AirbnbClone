00de61eb0ab15317a0725a55da055090645af04a
waleery <walenda1997@gmail.com>
2024-04-19 15:50:16 +0200

    feat(Tabs): change active tab tint color

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,7 +1,14 @@
+import Colors from "@/constants/Colors";
 import { Tabs } from "expo-router";
+
+
 const Layout = () => {
     return (
-        <Tabs>
+        <Tabs
+            screenOptions={{
+                tabBarActiveTintColor: Colors.primary,
+            }}
+        >
             <Tabs.Screen name="Index" options={{ tabBarLabel: "Explore" }} />
         </Tabs>
     );
