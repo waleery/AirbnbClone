295e5f8fab8f8c8fe1a53d736fe89c7596602e0a
waleery <walenda1997@gmail.com>
2024-04-19 15:46:19 +0200

    feat(Tabs): change label of index tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,9 @@
-import { Tabs } from 'expo-router'
+import { Tabs } from "expo-router";
 const Layout = () => {
-  return (
-    <Tabs>
-        
-    </Tabs>
-  )
-}
-export default Layout
+    return (
+        <Tabs>
+            <Tabs.Screen name="Index" options={{ tabBarLabel: "Explore" }} />
+        </Tabs>
+    );
+};
+export default Layout;
