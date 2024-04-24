f90995b233196f4a8ccb574005e01d61c6b27dfc
waleery <walenda1997@gmail.com>
2024-04-24 13:55:05 +0200

    feat: add buttons to login with phone/apple/google/facebook

--- app/(modals)/login.tsx
+++ app/(modals)/login.tsx
@@ -1,6 +1,7 @@
 import { defaultStyles } from "@/constants/Styles";
 import colors from "@/constants/colors";
 import { useWarmUpBrowser } from "@/hooks/useWarmUpBrowser";
+import { Ionicons } from "@expo/vector-icons";
 import {
     View,
     Text,
@@ -26,6 +27,33 @@ const Page = () => {
                 <Text style={styles.separatorText}>or</Text>
                 <View style={styles.separatorLine} />
             </View>
+
+            <View style={styles.containerPadding}>
+                <TouchableOpacity style={defaultStyles.btnOutline}>
+                    <Ionicons name="call-outline" style={defaultStyles.btnIcon} size={24}/>
+                    <Text style={defaultStyles.btnOutlineText}>
+                        Continue with Phone
+                    </Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={defaultStyles.btnOutline}>
+                    <Ionicons name="logo-apple" style={defaultStyles.btnIcon} size={24}/>
+                    <Text style={defaultStyles.btnOutlineText}>
+                        Continue with Apple
+                    </Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={defaultStyles.btnOutline}>
+                    <Ionicons name="logo-google" style={defaultStyles.btnIcon} size={24}/>
+                    <Text style={defaultStyles.btnOutlineText}>
+                        Continue with Google
+                    </Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={defaultStyles.btnOutline}>
+                    <Ionicons name="logo-facebook" style={defaultStyles.btnIcon} size={24}/>
+                    <Text style={defaultStyles.btnOutlineText}>
+                        Continue with Facebook
+                    </Text>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 };
@@ -35,6 +63,9 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: "#fff",
         padding: 26,
+    },
+    containerPadding:{
+        gap:15
     },
     separatorView: {
         flexDirection: "row",
