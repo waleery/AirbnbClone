454b7705e44da75c75c2537acdbc21b141b7762f
waleery <walenda1997@gmail.com>
2024-04-24 12:25:53 +0200

    feat(Constants): add default styles const with container and input field

---
+++ constants/Styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export const defaultStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#FDFFFF",
+    },
+    inputField: {
+        height: 44,
+        borderWidth: 1,
+        borderColor: "#ABABAB",
+        borderRadius: 8,
+        padding: 10,
+        backgroundColor: "#fff",
+    },
+});
