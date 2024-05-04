4edaa32fd97e6e28e87b3c3732717c617972d3df
waleery <walenda1997@gmail.com>
2024-05-04 17:08:20 +0200

    feat(Listing): load listings from mock data

--- app/(tabs)/Index.tsx
+++ app/(tabs)/Index.tsx
@@ -2,21 +2,28 @@ import ExploreHeader from "@/components/ExploreHeader";
 import Listings from "@/components/Listings";
 import { accomodation_categories } from "@/constants/categories";
 import { Stack } from "expo-router";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { View } from "react-native";
+import listingsData from "@/assets/data/airbnb-listings.json";
 
 const Page = () => {
+    const [category, setCategory] = useState(accomodation_categories[0].name);
+    const listings = useMemo(() => listingsData as any, []);
 
-    const [category, setCategory] = useState(accomodation_categories[0].name)
-
-    const onDataChanged = (category: string)=> {
-        setCategory(category)
-    }
+    const onDataChanged = (category: string) => {
+        setCategory(category);
+    };
 
     return (
         <View style={{ flex: 1 }}>
-            <Stack.Screen options={{ header: () => <ExploreHeader onCategoryChanged={onDataChanged}/> }} />
-            <Listings category={category} listings={[]} />
+            <Stack.Screen
+                options={{
+                    header: () => (
+                        <ExploreHeader onCategoryChanged={onDataChanged} />
+                    ),
+                }}
+            />
+            <Listings category={category} listings={listings} />
         </View>
     );
 };
--- components/Listings.tsx
+++ components/Listings.tsx
@@ -1,25 +1,35 @@
+import { Link } from "expo-router";
 import { useEffect, useRef, useState } from "react";
-import { View, Text, FlatList } from "react-native";
-
+import { View, Text, FlatList, ListRenderItem } from "react-native";
 interface Props {
     listings: any[];
     category: string;
 }
 const Listings = ({ category, listings: items }: Props) => {
+
     const [loading, setLoading] = useState(false);
     const listRef = useRef<FlatList>(null);
+
     useEffect(() => {
         setLoading(true);
 
         setTimeout(() => {
             setLoading(false);
         }, 200);
-        console.log("RELOAD LISTINGS");
+
+        console.log("RELOAD LISTINGS, listings length:", items.length);
     }, [category]);
 
+    const renderRow: ListRenderItem<any> = ({ item }) => {
+        return <Link href={`/listing/${item.id}`}>Go there</Link>;
+    };
     return (
         <View>
-            <FlatList data={loading ? [] : items} ref={listRef}  renderItem={()=>()}/>
+            <FlatList
+                data={loading ? [] : items}
+                ref={listRef}
+                renderItem={renderRow}
+            />
         </View>
     );
 };
