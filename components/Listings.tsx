16c0b73f5033e73c9e9b0761be272d6fc9ef90a8
waleery <walenda1997@gmail.com>
2024-05-05 23:37:37 +0200

    feat(Listing): display image of each listing

--- components/Listings.tsx
+++ components/Listings.tsx
@@ -1,13 +1,20 @@
 import { Listing } from "@/types/listing";
 import { Link } from "expo-router";
 import { useEffect, useRef, useState } from "react";
-import { View, Text, FlatList, ListRenderItem } from "react-native";
+import {
+    View,
+    Text,
+    FlatList,
+    ListRenderItem,
+    TouchableOpacity,
+    StyleSheet,
+    Image,
+} from "react-native";
 interface Props {
     listings: Listing[];
     category: string;
 }
 const Listings = ({ category, listings: items }: Props) => {
-
     const [loading, setLoading] = useState(false);
     const listRef = useRef<FlatList>(null);
 
@@ -22,7 +29,15 @@ const Listings = ({ category, listings: items }: Props) => {
     }, [category]);
 
     const renderRow: ListRenderItem<Listing> = ({ item }) => {
-        return <Link href={`/listing/${item.id}`}>Go there</Link>;
+        return (
+            <Link href={`/listing/${item.id}`} asChild>
+                <TouchableOpacity>
+                    <View style={styles.listing}>
+                        <Image source={{uri: item.medium_url!}} style={styles.image} />
+                    </View>
+                </TouchableOpacity>
+            </Link>
+        );
     };
     return (
         <View>
@@ -34,4 +49,15 @@ const Listings = ({ category, listings: items }: Props) => {
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    listing:{
+        padding:16
+    },
+    image:{
+        width:'100%',
+        height:300,
+        borderRadius:10
+    }
+})
 export default Listings;
