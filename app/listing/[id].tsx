7646a989dc4b4b7a664331b9caa332712a2b41c9
waleery <walenda1997@gmail.com>
2024-05-08 23:13:45 +0200

    feat(Listing): add finding the listing from id

--- app/listing/[id].tsx
+++ app/listing/[id].tsx
@@ -1,9 +1,11 @@
 import { useLocalSearchParams } from "expo-router";
 import { View, Text } from "react-native";
-
+import listingsData from '@/assets/data/airbnb-listings.json'
+import { Listing } from "@/types/listing";
 const Page = () => {
     const { id } = useLocalSearchParams<{ id: string }>();
-    console.log(id);
+    const listing = (listingsData as Listing[]).find((item) => item.id == id)
+    console.log(listing)
     return (
         <View>
             <Text>Page</Text>
