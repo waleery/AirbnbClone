e1d21614670c86111d0c6001d1f8ea5eaabd547e
waleery <walenda1997@gmail.com>
2024-05-08 23:52:35 +0200

    feat(Listing): display accomodation image at listing page

--- app/listing/[id].tsx
+++ app/listing/[id].tsx
@@ -1,15 +1,36 @@
 import { useLocalSearchParams } from "expo-router";
-import { View, Text } from "react-native";
-import listingsData from '@/assets/data/airbnb-listings.json'
+import { View, Text, StyleSheet, Dimensions } from "react-native";
+import listingsData from "@/assets/data/airbnb-listings.json";
 import { Listing } from "@/types/listing";
+import Animated from "react-native-reanimated";
+
+const IMG_HEIGHT = 300;
+const { width } = Dimensions.get("window");
+
 const Page = () => {
     const { id } = useLocalSearchParams<{ id: string }>();
-    const listing = (listingsData as Listing[]).find((item) => item.id == id)
-    console.log(listing)
+    const listing = (listingsData as Listing[]).find((item) => item.id == id);
+
     return (
-        <View>
-            <Text>Page</Text>
+        <View style={styles.container}>
+            <Animated.ScrollView>
+                <Animated.Image
+                    source={{ uri: listing?.xl_picture_url! }}
+                    style={styles.image}
+                />
+            </Animated.ScrollView>
         </View>
     );
 };
 export default Page;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "white",
+    },
+    image: {
+        height: IMG_HEIGHT,
+        width: width,
+    },
+});
