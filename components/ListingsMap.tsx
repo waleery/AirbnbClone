a111717a6c5b851019e0a76b5efe1e396f68412e
waleery <walenda1997@gmail.com>
2024-05-19 23:27:06 +0200

    feat(Map): add initial region

--- components/ListingsMap.tsx
+++ components/ListingsMap.tsx
@@ -6,10 +6,17 @@ interface Props {
     listings: ListingGeo[];
 }
 
+const INITIAL_REGION = {
+    latitude: 51,
+    longitude: 11,
+    latitudeDelta: 9,
+    longitudeDelta: 9,
+  };
+
 const ListingsMap = ({ listings }: Props) => {
     return (
         <View style={styles.container}>
-            <MapView style={styles.map}  provider={PROVIDER_GOOGLE} showsUserLocation={true} showsMyLocationButton/>
+            <MapView style={styles.map}  provider={PROVIDER_GOOGLE} showsUserLocation={true} showsMyLocationButton initialRegion={INITIAL_REGION}/>
         </View>
     );
 };
