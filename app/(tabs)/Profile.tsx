7bbd6c3f430f8f1d3075953a1e17dbcb31319b30
waleery <walenda1997@gmail.com>
2024-05-27 23:59:37 +0200

    feat: get user data from clerk

--- app/(tabs)/Profile.tsx
+++ app/(tabs)/Profile.tsx
@@ -1,8 +1,22 @@
-import { useAuth } from "@clerk/clerk-expo";
+import { useAuth, useUser } from "@clerk/clerk-expo";
 import { Link } from "expo-router";
+import { useEffect, useState } from "react";
 import { View, Text, Button } from "react-native";
 const Page = () => {
     const { signOut, isSignedIn } = useAuth();
+
+    const {user} = useUser()
+    const [firstName, setFirstName]= useState(user?.firstName)
+    const [lastName, setLastName]= useState(user?.lastName)
+    const [email, setEmail]= useState(user?.emailAddresses)
+    const [edit, setEdit]= useState(false)
+
+    useEffect(() => {
+        if(!user){
+            return
+        }
+    }, [user]);
+    
     return (
         <View>
             <Button title="Log out" onPress={() => signOut()} />
