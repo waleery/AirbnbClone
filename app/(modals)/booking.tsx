9e9a398b396f7f51017a362137dae749402e7a4f
waleery <walenda1997@gmail.com>
2024-05-30 11:21:45 +0200

    feat(Filter): add paddingTop to filter page

--- app/(modals)/booking.tsx
+++ app/(modals)/booking.tsx
@@ -1,6 +1,5 @@
 import { View, Text, StyleSheet } from "react-native";
 import { BlurView } from "expo-blur";
-import { defaultStyles } from "@/constants/Styles";
 const Page = () => {
     return (
         <BlurView intensity={70} style={styles.container} tint="light">
@@ -12,5 +11,6 @@ export default Page;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        paddingTop:100,
     },
 });
