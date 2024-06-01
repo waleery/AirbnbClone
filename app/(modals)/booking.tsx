f33bc461f8fde0ec3b2ab678f69e8eab2a1b6050
waleery <walenda1997@gmail.com>
2024-06-01 23:59:37 +0200

    feat(Filter): add footer to filter page

--- app/(modals)/booking.tsx
+++ app/(modals)/booking.tsx
@@ -1,16 +1,48 @@
 import { View, Text, StyleSheet } from "react-native";
 import { BlurView } from "expo-blur";
+import Animated, { SlideInDown } from "react-native-reanimated";
+import { defaultStyles } from "@/constants/Styles";
+import { useRouter } from "expo-router";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { Ionicons } from "@expo/vector-icons";
 const Page = () => {
-    return (
-        <BlurView intensity={70} style={styles.container} tint="light">
-            <Text>Booking</Text>
-        </BlurView>
-    );
+  const router = useRouter();
+
+  const onClearAll = () => {
+
+  }
+  return (
+    <BlurView intensity={70} style={styles.container} tint="light">
+      <Text>Booking</Text>
+
+      <Animated.View
+        style={defaultStyles.footer}
+        entering={SlideInDown.delay(200)}
+      >
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <TouchableOpacity onPress={onClearAll} style={{justifyContent:'center'}}>
+            <Text style={{fontSize:18, fontWeight:'500', textDecorationLine:'underline'}}>Clear all</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={router.back} style={[defaultStyles.btn, {paddingRight:20, paddingLeft:50}]}>
+         <Ionicons name="search-outline" size={24} color={"#fff"}  style={defaultStyles.btnIcon}/>
+            <Text style={defaultStyles.btnText}>Clear all</Text>
+          </TouchableOpacity>
+        </View>
+      </Animated.View>
+    </BlurView>
+  );
 };
 export default Page;
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        paddingTop:100,
-    },
+  container: {
+    flex: 1,
+    paddingTop: 100,
+  },
 });
--- components/ModalHeaderText.tsx
+++ components/ModalHeaderText.tsx
@@ -3,39 +3,43 @@ import { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 const ModalHeaderText = () => {
-    const [active, setActive] = useState(0);
-    return (
-        <View
-            style={{ flexDirection: "row", justifyContent: "center", gap: 10 }}
+  const [active, setActive] = useState(0);
+  return (
+    <View style={{ flexDirection: "row", justifyContent: "center", gap: 10 }}>
+      <TouchableOpacity onPress={() => setActive(0)}>
+        <Text
+          style={[
+            styles.headerText,
+            {
+              color: active === 0 ? "#000" : colors.grey,
+              textDecorationLine: active === 0 ? "underline" : "none",
+            },
+          ]}
         >
-            <TouchableOpacity onPress={() => setActive(0)}>
-                <Text
-                    style={[
-                        styles.headerText,
-                        { color: active === 0 ? "#000" : colors.grey, textDecorationLine: active === 0 ? 'underline' : 'none' },
-                    ]}
-                >
-                    Stays
-                </Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => setActive(1)}>
-                <Text
-                    style={[
-                        styles.headerText,
-                        { color: active === 1 ? "#000" : colors.grey, textDecorationLine: active === 1 ? 'underline' : 'none' },
-                    ]}
-                >
-                    Experiences
-                </Text>
-            </TouchableOpacity>
-        </View>
-    );
+          Stays
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => setActive(1)}>
+        <Text
+          style={[
+            styles.headerText,
+            {
+              color: active === 1 ? "#000" : colors.grey,
+              textDecorationLine: active === 1 ? "underline" : "none",
+            },
+          ]}
+        >
+          Experiences
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 export default ModalHeaderText;
 
 const styles = StyleSheet.create({
-    headerText: {
-        fontWeight: "500",
-        fontSize: 18,
-    },
+  headerText: {
+    fontWeight: "500",
+    fontSize: 18,
+  },
 });
