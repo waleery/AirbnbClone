d4cd6ceff0ce7ac50bed0a334230827007b5813b
waleery <walenda1997@gmail.com>
2024-06-07 23:39:34 +0200

    refactor(App): extract styles of headerLeft of Filter component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,133 +1,128 @@
-import { Ionicons } from "@expo/vector-icons";
-import FontAwesome from "@expo/vector-icons/FontAwesome";
-import { useFonts } from "expo-font";
-import { Stack, useRouter } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
-import { TouchableOpacity } from "react-native";
-import * as SecureStore from "expo-secure-store";
-import { ClerkProvider, useAuth } from "@clerk/clerk-expo";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { defaultStyles } from "@/constants/Styles";
-import ModalHeaderText from "@/components/ModalHeaderText";
-import colors from "@/constants/Colors";
-const CLERK_PUBLISAHBLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+import { Ionicons } from '@expo/vector-icons'
+import FontAwesome from '@expo/vector-icons/FontAwesome'
+import { useFonts } from 'expo-font'
+import { Stack, useRouter } from 'expo-router'
+import * as SplashScreen from 'expo-splash-screen'
+import { useEffect } from 'react'
+import { StyleSheet, TouchableOpacity } from 'react-native'
+import * as SecureStore from 'expo-secure-store'
+import { ClerkProvider, useAuth } from '@clerk/clerk-expo'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
+import { defaultStyles } from '@/constants/Styles'
+import ModalHeaderText from '@/components/ModalHeaderText'
+import colors from '@/constants/Colors'
+const CLERK_PUBLISAHBLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY
 
 const tokenCache = {
-    async getToken(key: string) {
-        try {
-            return SecureStore.getItemAsync(key);
-        } catch (error) {
-            return null;
-        }
-    },
-    async saveToken(key: string, value: string) {
-        try {
-            return SecureStore.setItemAsync(key, value);
-        } catch (error) {
-            return;
-        }
-    },
-};
+  async getToken(key: string) {
+    try {
+      return SecureStore.getItemAsync(key)
+    } catch (error) {
+      return null
+    }
+  },
+  async saveToken(key: string, value: string) {
+    try {
+      return SecureStore.setItemAsync(key, value)
+    } catch (error) {
+      return
+    }
+  },
+}
 
 export {
-    // Catch any errors thrown by the Layout component.
-    ErrorBoundary,
-} from "expo-router";
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from 'expo-router'
 
 export const unstable_settings = {
-    // Ensure that reloading on `/modal` keeps a back button present.
-    initialRouteName: "(tabs)",
-};
+  // Ensure that reloading on `/modal` keeps a back button present.
+  initialRouteName: '(tabs)',
+}
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
-    const [loaded, error] = useFonts({
-        SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-        ...FontAwesome.font,
-    });
+  const [loaded, error] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    ...FontAwesome.font,
+  })
 
-    // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-    useEffect(() => {
-        if (error) throw error;
-    }, [error]);
+  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+  useEffect(() => {
+    if (error) throw error
+  }, [error])
 
-    useEffect(() => {
-        if (loaded) {
-            SplashScreen.hideAsync();
-        }
-    }, [loaded]);
-
-    if (!loaded) {
-        return null;
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync()
     }
+  }, [loaded])
 
-    return (
-        <ClerkProvider
-            publishableKey={CLERK_PUBLISAHBLE_KEY!}
-            tokenCache={tokenCache}
-        >
-            <GestureHandlerRootView style={defaultStyles.flex}>
-                <RootLayoutNav />
-            </GestureHandlerRootView>
-        </ClerkProvider>
-    );
+  if (!loaded) {
+    return null
+  }
+
+  return (
+    <ClerkProvider publishableKey={CLERK_PUBLISAHBLE_KEY!} tokenCache={tokenCache}>
+      <GestureHandlerRootView style={defaultStyles.flex}>
+        <RootLayoutNav />
+      </GestureHandlerRootView>
+    </ClerkProvider>
+  )
 }
 
 function RootLayoutNav() {
-    const router = useRouter();
-    const { isLoaded, isSignedIn } = useAuth();
+  const router = useRouter()
+  const { isLoaded, isSignedIn } = useAuth()
 
-    useEffect(() => {
-        if (isLoaded && !isSignedIn) {
-            router.push("/(modals)/login");
-        }
-    }, [isLoaded]);
+  useEffect(() => {
+    if (isLoaded && !isSignedIn) {
+      router.push('/(modals)/login')
+    }
+  }, [isLoaded])
 
-    return (
-        <Stack>
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            <Stack.Screen
-                name="(modals)/login"
-                options={{
-                    presentation: "modal",
-                    title: "Log in or sign up",
-                    headerLeft: () => (
-                        <TouchableOpacity onPress={() => router.back()}>
-                            <Ionicons name="close-outline" size={28} />
-                        </TouchableOpacity>
-                    ),
-                }}
-            />
-            <Stack.Screen
-                name="listing/[id]"
-                options={{ headerTitle: "", headerTransparent: true }}
-            />
-            <Stack.Screen
-                name="(modals)/booking"
-                options={{
-                    presentation: "transparentModal",
-                    headerTransparent: true,
-                    animation: "fade",
-                    headerTitle: () => <ModalHeaderText />,
-                    headerLeft: () => (
-                        <TouchableOpacity
-                            onPress={() => router.back()}
-                            style={{
-                                backgroundColor: "white",
-                                borderColor: colors.grey,
-                                borderRadius: 20,
-                                borderWidth: 1,
-                                padding:4
-                            }}
-                        >
-                            <Ionicons name="close-outline" size={28} />
-                        </TouchableOpacity>
-                    ),
-                }}
-            />
-        </Stack>
-    );
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="(modals)/login"
+        options={{
+          presentation: 'modal',
+          title: 'Log in or sign up',
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => router.back()}>
+              <Ionicons name="close-outline" size={28} />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <Stack.Screen name="listing/[id]" options={{ headerTitle: '', headerTransparent: true }} />
+      <Stack.Screen
+        name="(modals)/booking"
+        options={{
+          presentation: 'transparentModal',
+          headerTransparent: true,
+          animation: 'fade',
+          headerTitle: () => <ModalHeaderText />,
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => router.back()} style={styles.headerLeft}>
+              <Ionicons name="close-outline" size={28} />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+    </Stack>
+  )
 }
+
+const styles = StyleSheet.create({
+  headerLeft: {
+    backgroundColor: 'white',
+    borderColor: colors.grey,
+    borderRadius: 20,
+    borderWidth: 1,
+    padding: 4,
+  },
+})
