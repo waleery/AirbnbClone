6b3b357b99eddac72718e4fd1ce589e6747e8561
waleery <walenda1997@gmail.com>
2024-06-08 23:32:33 +0200

    feat(Filter): add guestGroups array

--- app/(modals)/booking.tsx
+++ app/(modals)/booking.tsx
@@ -11,6 +11,29 @@ import { places } from '@/assets/data/places'
 
 const AnimatedTouchableOpacity = Animated.createAnimatedComponent(TouchableOpacity)
 
+const guestsGroups = [
+  {
+    name: 'Adults',
+    text: 'Ages 13 or above',
+    count: 0,
+  },
+  {
+    name: 'Children',
+    text: 'Ages 2-12',
+    count: 0,
+  },
+  {
+    name: 'Infants',
+    text: 'Under 2',
+    count: 0,
+  },
+  {
+    name: 'Pest',
+    text: 'Pets allowed',
+    count: 0,
+  }
+]
+
 //@ts-ignore
 import DatePicker from 'react-native-modern-datepicker'
 
