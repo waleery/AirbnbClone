822dd6a18ccee556cd92b206ba18e911c6126b35
waleery <walenda1997@gmail.com>
2024-06-09 09:55:28 +0200

    feat(Filter): add styles to text with count

--- app/(modals)/booking.tsx
+++ app/(modals)/booking.tsx
@@ -167,7 +167,7 @@ const Page = () => {
                     <TouchableOpacity>
                       <Ionicons name="remove-circle-outline" size={26} color={groups[index].count > 0 ? Colors.grey : "#cdcdcd"}/>
                     </TouchableOpacity>
-                    <Text>{item.count}</Text>
+                    <Text style={{fontSize:16, textAlign:'center', minWidth:20}}>{item.count}</Text>
                     <TouchableOpacity onPress={() => {
                       const newGroups = [...groups]
                       newGroups[index].count++ 
