dd57dc416829c9f74fb76c066ef3fa34434d63bd
waleery <walenda1997@gmail.com>
2024-06-15 12:54:30 +0200

    feat(Store): add filtering listings by stay days

--- store/listingsStore.ts
+++ store/listingsStore.ts
@@ -1,15 +1,24 @@
 // atoms.js
-import { Listing } from '@/types/listing';
-import { atom } from 'jotai';
-import listingsData from "@/assets/data/airbnb-listings.json";
+import { Listing } from '@/types/listing'
+import { atom } from 'jotai'
+import listingsData from '@/assets/data/airbnb-listings.json'
 
-export const listingsAtom = atom<Listing[]>(listingsData as Listing[]);
+export const listingsAtom = atom<Listing[]>(listingsData as Listing[])
 
 // Atom do przechowywania filtra guests_included
-export const guestsIncludedFilterAtom = atom<number>(1);
+export const guestsIncludedFilterAtom = atom<number>(1)
+
+export const daysStayFilterAtom = atom<number>(1)
 
 export const filteredListingsAtom = atom((get) => {
-    const listings = get(listingsAtom);
-    const guestsIncluded = get(guestsIncludedFilterAtom);
-    return listings.filter(listing => listing.guests_included >= guestsIncluded);
-  });
+  const listings = get(listingsAtom)
+  const guestsIncluded = get(guestsIncludedFilterAtom)
+  const daysStay = get(daysStayFilterAtom)
+
+  return listings.filter(
+    (listing) =>
+      listing.guests_included >= guestsIncluded &&
+      listing.minimum_nights <= daysStay &&
+      listing.maximum_nights >= daysStay
+  )
+})
