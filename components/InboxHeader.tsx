abf66ed7de5400edfefc1048999ac9cedb76eb43
waleery <walenda1997@gmail.com>
2024-06-26 15:45:25 +0200

    feat(Filter): Display buttons in InboxHeader

--- components/InboxHeader.tsx
+++ components/InboxHeader.tsx
@@ -1,15 +1,37 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { Ionicons } from '@expo/vector-icons'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import Colors from '@/constants/Colors'
 
 const InboxHeader = () => {
   return (
-    <SafeAreaView edges={['top']}>
-        <Text>Header</Text>
+    <SafeAreaView edges={['top']} style={{ paddingHorizontal: 20 }}>
+      <View style={{ display: 'flex', flexDirection: 'row-reverse', gap: 10 }}>
+        <TouchableOpacity style={styles.topBtn}>
+          <Ionicons name="search" size={24} />
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.topBtn}>
+          <Ionicons name="options-outline" size={24} />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   )
 }
 
 export default InboxHeader
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  topBtn: {
+    padding: 10,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: Colors.grey,
+    borderRadius: 24,
+    elevation: 2,
+    shadowColor: '#000',
+    shadowOpacity: 0.07,
+    shadowRadius: 8,
+  },
+})
