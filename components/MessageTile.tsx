4533f69a1c4fe0cdae8d503df4da18dd95d6a078
waleery <walenda1997@gmail.com>
2024-07-13 18:10:50 +0200

    fix(Inbox): Fix swipable work

--- components/MessageTile.tsx
+++ components/MessageTile.tsx
@@ -1,13 +1,21 @@
-import { Button, Image, StyleSheet, Text, View } from 'react-native'
+import { Animated, Button, Image, Pressable, StyleSheet, Text, View } from 'react-native'
 import React, { useMemo } from 'react'
 import { useAtomValue } from 'jotai'
 import { listingsAtom } from '@/store/listingsStore'
 import { Conversation } from '@/types/messages'
 import { getDate, getMonth, getYear } from 'date-fns'
 import { defaultStyles } from '@/constants/Styles'
-import { FontAwesome5 } from '@expo/vector-icons'
-import { Swipeable } from 'react-native-gesture-handler'
-import Animated, { interpolate } from 'react-native-reanimated'
+import { AntDesign, Entypo, FontAwesome5 } from '@expo/vector-icons'
+import { RectButton, Swipeable } from 'react-native-gesture-handler'
+import { AnimatedInterpolation } from 'react-native-gesture-handler/lib/typescript/components/Swipeable'
+
+// Definiowanie typów kluczy
+type IconKey = 'Archive' | 'Star'
+
+const iconMap: Record<IconKey, JSX.Element> = {
+  Archive: <Entypo name="archive" size={20} color="white" />,
+  Star: <AntDesign name="star" size={20} color="white" />,
+}
 
 const MessageTile = ({ conversation }: { conversation: Conversation }) => {
   const accomodation = useAtomValue(listingsAtom).find(
@@ -50,24 +58,34 @@ const MessageTile = ({ conversation }: { conversation: Conversation }) => {
       break
   }
 
-  const renderRightActions = () => {
-    const trans = interpolate(0, [-20, 0, 0, 1], [20, 0, 0, 1])
+  const renderRightAction = (
+    text: IconKey,
+    color: string,
+    x: number,
+    progress: AnimatedInterpolation
+  ) => {
+    const trans = progress.interpolate({
+      inputRange: [0, 1],
+      outputRange: [x, 0],
+    })
 
     return (
-      <Animated.View style={styles.rightAction}>
-        <Animated.Text
-          style={[
-            styles.actionText,
-            {
-              transform: [{ translateX: trans }],
-            },
-          ]}
-        >
-          Archive
-        </Animated.Text>
+      <Animated.View style={{ flex: 1, transform: [{ translateX: trans }] }}>
+        <Pressable style={[styles.rightAction, { backgroundColor: color }]}>
+          {iconMap[text]}
+          <Text style={styles.actionText}>{text}</Text>
+        </Pressable>
       </Animated.View>
     )
   }
+
+  const renderRightActions = (progress: AnimatedInterpolation) => (
+    <View style={{ width: 128, flexDirection: 'row' }}>
+      {renderRightAction('Star', '#00A699', 128, progress)}
+      {renderRightAction('Archive', '#484848', 64, progress)}
+    </View>
+  )
+
   return (
     <Swipeable
       renderRightActions={renderRightActions}
@@ -75,56 +93,56 @@ const MessageTile = ({ conversation }: { conversation: Conversation }) => {
       leftThreshold={40}
       rightThreshold={40}
       childrenContainerStyle={styles.messagesContainer}
-      containerStyle={{overflow:'visible'}}
+      containerStyle={{ overflow: 'visible' }}
     >
-        <View>
-          {conversation.customer_service ? (
-            <View style={styles.customerServiceImage}>
-              <FontAwesome5 name="airbnb" size={30} color={'white'} />
-            </View>
-          ) : (
-            <Image source={{ uri: accomodation?.medium_url! }} style={styles.image} />
-          )}
-          {conversation.hosts?.map((host, index) => {
-            if (index < 2) {
-              return (
-                <Image
-                  source={{ uri: host?.image }}
-                  style={[
-                    styles.hostImage,
-                    {
-                      width: hostImageSize,
-                      height: hostImageSize,
-                      transform: [
-                        { translateY: hostImagePositions[index]?.translateY || 0 },
-                        { translateX: hostImagePositions[index]?.translateX || 0 },
-                      ],
-                    },
-                  ]}
-                />
-              )
-            }
-            return null
-          })}
-        </View>
-        <View style={styles.textContainer}>
-          <View style={styles.firstLine}>
-            <Text style={defaultStyles.thinText}>
-              {conversation.customer_service ? 'Airbnb Customer Service' : accomodation?.host_name}
-            </Text>
-            <Text style={defaultStyles.thinText}>{formatDateLastMessage}</Text>
+      <View>
+        {conversation.customer_service ? (
+          <View style={styles.customerServiceImage}>
+            <FontAwesome5 name="airbnb" size={30} color={'white'} />
           </View>
-          <Text style={defaultStyles.thinText} numberOfLines={1} ellipsizeMode="tail">
-            {lastMessage.message}
+        ) : (
+          <Image source={{ uri: accomodation?.medium_url! }} style={styles.image} />
+        )}
+        {conversation.hosts?.map((host, index) => {
+          if (index < 2) {
+            return (
+              <Image
+                source={{ uri: host?.image }}
+                style={[
+                  styles.hostImage,
+                  {
+                    width: hostImageSize,
+                    height: hostImageSize,
+                    transform: [
+                      { translateY: hostImagePositions[index]?.translateY || 0 },
+                      { translateX: hostImagePositions[index]?.translateX || 0 },
+                    ],
+                  },
+                ]}
+              />
+            )
+          }
+          return null
+        })}
+      </View>
+      <View style={styles.textContainer}>
+        <View style={styles.firstLine}>
+          <Text style={defaultStyles.thinText}>
+            {conversation.customer_service ? 'Airbnb Customer Service' : accomodation?.host_name}
           </Text>
-          <View>
-            <Text style={defaultStyles.thinText}>
-              {conversation.customer_service
-                ? 'Welcome'
-                : `${conversation.accomodation_date}  · ${accomodation?.city}`}
-            </Text>
-          </View>
+          <Text style={defaultStyles.thinText}>{formatDateLastMessage}</Text>
         </View>
+        <Text style={defaultStyles.thinText} numberOfLines={1} ellipsizeMode="tail">
+          {lastMessage.message}
+        </Text>
+        <View>
+          <Text style={defaultStyles.thinText}>
+            {conversation.customer_service
+              ? 'Welcome'
+              : `${conversation.accomodation_date}  · ${accomodation?.city}`}
+          </Text>
+        </View>
+      </View>
     </Swipeable>
   )
 }
@@ -174,11 +192,11 @@ const styles = StyleSheet.create({
   },
   rightAction: {
     justifyContent: 'center',
+    alignItems: 'center',
     flex: 1,
-    alignItems: 'flex-end',
+    //alignItems: 'flex-end',
   },
   actionText: {
-    padding: 20,
-    borderWidth: 2,
+    color: 'white',
   },
 })
