716a432634293e66bca2f7182715fa6865085a78
waleery <walenda1997@gmail.com>
2024-07-19 13:03:01 +0200

    feat(Wishlist): Display wishlist title

--- app/(tabs)/wishlists.tsx
+++ app/(tabs)/wishlists.tsx
@@ -1,9 +1,29 @@
-import { View, Text } from 'react-native'
+import { defaultStyles } from '@/constants/Styles'
+import { View, Text, StyleSheet } from 'react-native'
 const Page = () => {
   return (
-    <View>
-      <Text>Wishlists</Text>
+    <View style={defaultStyles.flex}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Wishlist</Text>
+      </View>
     </View>
   )
 }
 export default Page
+
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    //padding: 16,
+    gap: 20,
+
+    flex: 1,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: '500',
+    paddingTop:16,
+    paddingHorizontal:16
+  },
+})
