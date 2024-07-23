69b3ecc798304296506483c8804b209b06964661
waleery <walenda1997@gmail.com>
2024-07-23 23:02:46 +0200

    feat(Wishlist): Display boxes with id's of wishlist

--- app/(tabs)/wishlists.tsx
+++ app/(tabs)/wishlists.tsx
@@ -1,7 +1,13 @@
 import WishlistHeader from '@/components/WishlistHeader'
 import { defaultStyles } from '@/constants/Styles'
-import { Stack } from 'expo-router'
-import { View, Text, StyleSheet } from 'react-native'
+import { Link, Stack } from 'expo-router'
+import { View, Text, StyleSheet, ListRenderItem } from 'react-native'
+import wishlistData from '@/assets/data/wishlist.json'
+import { Wishlist } from '@/types/whishlist'
+import { FlatList } from 'react-native-gesture-handler'
+import { TouchableOpacity } from '@gorhom/bottom-sheet'
+const wishlists: Wishlist[] = wishlistData as Wishlist[]
+
 const Page = () => {
   return (
     <View style={defaultStyles.flex}>
@@ -12,12 +18,42 @@ const Page = () => {
       />
       <View style={styles.container}>
         <Text style={styles.title}>Wishlist</Text>
+        <WishlistTiles />
       </View>
     </View>
   )
 }
 export default Page
 
+const WishlistTiles = () => {
+  const wishlists: Wishlist[] = wishlistData as Wishlist[]
+
+  const renderRow: ListRenderItem<Wishlist> = ({ item }) => (
+    <View>
+      <View style={styles.tileBox}>
+        {item.list.map((listItem) => (
+          <Link href={`/listing/${listItem.id}`} asChild>
+            <TouchableOpacity>
+              <Text key={listItem.id} style={styles.listItemId}>
+                {listItem.id}
+              </Text>
+            </TouchableOpacity>
+          </Link>
+        ))}
+      </View>
+
+      <Text style={styles.itemName}>{item.name}</Text>
+    </View>
+  )
+
+  return (
+    <FlatList
+      data={wishlists}
+      renderItem={renderRow}
+      numColumns={2}
+    />
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -30,5 +66,9 @@ const styles = StyleSheet.create({
     fontWeight: '500',
     paddingTop: 16,
     paddingHorizontal: 16,
+  },
+
+  tileBox: {
+    borderWidth: 2,
   },
 })
