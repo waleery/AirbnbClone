44252c8af66f6859dd4a54731a1c7c0ff5e78b66
waleery <walenda1997@gmail.com>
2024-08-03 23:16:47 +0200

    feat(Wishlist): add function to toogle edit mode

--- store/wishlistStore.ts
+++ store/wishlistStore.ts
@@ -3,3 +3,7 @@ import { atom, createStore } from "jotai";
 const store = createStore()
 
 export const wishlistEditMode = atom<boolean>(false)
+
+export const toogleWishlistEditMode = () => {
+    store.set(wishlistEditMode, !wishlistEditMode)
+}
