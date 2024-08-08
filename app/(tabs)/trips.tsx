6cc3e2160c25f4621c70fbbb4eb739500bc4f2dd
waleery <walenda1997@gmail.com>
2024-08-08 22:52:28 +0200

    feat(Trips): Add header

--- app/(tabs)/trips.tsx
+++ app/(tabs)/trips.tsx
@@ -1,9 +1,34 @@
-import { View, Text } from 'react-native'
+import { defaultStyles } from '@/constants/Styles'
+import { Stack } from 'expo-router'
+import { View, Text, StyleSheet } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 const Page = () => {
   return (
-    <View>
-      <Text>Trips</Text>
+    <View style={defaultStyles.flex}>
+      <Stack.Screen
+        options={{
+          headerShown: false, // Wyłączenie nagłówka
+        }}
+      />
+      <SafeAreaView edges={['top']} style={[ styles.container]}>
+        <Text style={styles.title}>Trips</Text>
+      </SafeAreaView>
     </View>
   )
 }
 export default Page
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    gap: 20,
+    flex: 1,
+    paddingTop:20
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: '500',
+    paddingTop: 16,
+    paddingHorizontal: 16,
+  },
+})
