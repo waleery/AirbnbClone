bdf8a2c21de6f82777c8527ad95d9d601d126956
waleery <walenda1997@gmail.com>
2024-08-09 23:48:13 +0200

    feat(Trips): add tile with info of no tirps booked yet

--- app/(tabs)/trips.tsx
+++ app/(tabs)/trips.tsx
@@ -1,7 +1,11 @@
+import Colors from '@/constants/Colors'
 import { defaultStyles } from '@/constants/Styles'
 import { Stack } from 'expo-router'
 import { View, Text, StyleSheet } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { FontAwesome } from '@expo/vector-icons'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+
 const Page = () => {
   return (
     <View style={defaultStyles.flex}>
@@ -10,8 +14,18 @@ const Page = () => {
           headerShown: false, // Wyłączenie nagłówka
         }}
       />
-      <SafeAreaView edges={['top']} style={[ styles.container]}>
+      <SafeAreaView edges={['top']} style={[styles.container]}>
         <Text style={styles.title}>Trips</Text>
+        <View style={styles.noTrips}>
+          <FontAwesome name="hand-o-up" size={30} color={Colors.primary} />
+          <Text style={styles.noTripsText1}>No trips booked...yet</Text>
+          <Text style={styles.noTripsText2}>
+            Time to dust off your bags and start planning your next adventure
+          </Text>
+          <TouchableOpacity style={[defaultStyles.btn, { paddingHorizontal:50}]}>
+            <Text style={[defaultStyles.btnText, { fontWeight: '500' }]}>Start searching</Text>
+          </TouchableOpacity>
+        </View>
       </SafeAreaView>
     </View>
   )
@@ -23,12 +37,32 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     gap: 20,
     flex: 1,
-    paddingTop:20
+    paddingTop: 20,
   },
   title: {
     fontSize: 30,
     fontWeight: '500',
     paddingTop: 16,
     paddingHorizontal: 16,
+  },
+  noTrips: {
+    marginHorizontal: 16,
+    padding: 16,
+
+    borderWidth: StyleSheet.hairlineWidth,
+    borderBlockColor: Colors.grey,
+    height: '35%',
+    borderRadius: 10,
+    alignItems: 'center',
+    gap: 20,
+  },
+  noTripsText1: {
+    fontSize: 18,
+    fontWeight: '600',
+  },
+  noTripsText2: {
+    fontWeight: '300',
+    fontSize: 13,
+    textAlign:'center'
   },
 })
