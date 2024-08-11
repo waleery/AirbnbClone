3522f5893d93fa6ac8ab511836a9954da0a2b772
waleery <walenda1997@gmail.com>
2024-08-11 23:58:00 +0200

    feat(Trips): add link to index page at tile with no planned trips

--- app/(tabs)/trips.tsx
+++ app/(tabs)/trips.tsx
@@ -1,9 +1,8 @@
 import Colors from '@/constants/Colors'
 import { defaultStyles } from '@/constants/Styles'
-import { Stack } from 'expo-router'
+import { Link, Stack } from 'expo-router'
 import { View, Text, StyleSheet, Image } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { FontAwesome } from '@expo/vector-icons'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 
 const Page = () => {
@@ -17,17 +16,16 @@ const Page = () => {
       <SafeAreaView edges={['top']} style={[styles.container]}>
         <Text style={styles.title}>Trips</Text>
         <View style={styles.noTrips}>
-          <Image
-            source={require('@/assets/data/hello.png')}
-            style={styles.helloIcon}
-          />
+          <Image source={require('@/assets/data/hello.png')} style={styles.helloIcon} />
           <Text style={styles.noTripsText1}>No trips booked...yet</Text>
           <Text style={styles.noTripsText2}>
             Time to dust off your bags and start planning your next adventure
           </Text>
-          <TouchableOpacity style={[defaultStyles.btn, { paddingHorizontal: 50 }]}>
-            <Text style={[defaultStyles.btnText, { fontWeight: '500' }]}>Start searching</Text>
-          </TouchableOpacity>
+          <Link href={`/`}>
+            <TouchableOpacity style={[defaultStyles.btn, { paddingHorizontal: 50 }]}>
+              <Text style={[defaultStyles.btnText, { fontWeight: '500' }]}>Start searching</Text>
+            </TouchableOpacity>
+          </Link>
         </View>
       </SafeAreaView>
     </View>
@@ -54,7 +52,7 @@ const styles = StyleSheet.create({
 
     borderWidth: StyleSheet.hairlineWidth,
     borderBlockColor: Colors.grey,
-    
+
     borderRadius: 10,
     alignItems: 'center',
     gap: 20,
@@ -68,9 +66,9 @@ const styles = StyleSheet.create({
     fontSize: 13,
     textAlign: 'center',
   },
-  helloIcon:{
-    marginVertical:8,
+  helloIcon: {
+    marginVertical: 8,
     width: 50,
     height: 50,
-  }
+  },
 })
