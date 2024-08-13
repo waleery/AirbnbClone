34ffc966e88d614392013fb94b375537da10b696
waleery <walenda1997@gmail.com>
2024-08-13 23:05:59 +0200

    refactor: explore tab

--- app/(tabs)/Index.tsx
+++ app/(tabs)/Index.tsx
@@ -1,40 +1,38 @@
-import ExploreHeader from "@/components/ExploreHeader";
-import Listings from "@/components/Listings";
-import { accomodation_categories } from "@/constants/categories";
-import { Stack } from "expo-router";
-import { useMemo, useState } from "react";
-import { View } from "react-native";
-import listingsDataGeo from "@/assets/data/airbnb-listings.geo.json";
-import { ListingGeo } from "@/types/listingGeo";
+import ExploreHeader from '@/components/ExploreHeader'
+import Listings from '@/components/Listings'
+import { accomodation_categories } from '@/constants/categories'
+import { Stack } from 'expo-router'
+import { useCallback, useMemo, useState } from 'react'
+import { View } from 'react-native'
+import listingsDataGeo from '@/assets/data/airbnb-listings.geo.json'
+import { ListingGeo } from '@/types/listingGeo'
 
-import ListingsMap from "@/components/ListingsMap";
-import ListingsBottomSheet from "@/components/ListingsBottomSheet";
-import { defaultStyles } from "@/constants/Styles";
-import { useAtom } from "jotai";
-import { filteredListingsAtom } from "@/store/listingsStore";
+import ListingsMap from '@/components/ListingsMap'
+import ListingsBottomSheet from '@/components/ListingsBottomSheet'
+import { defaultStyles } from '@/constants/Styles'
+import { useAtom } from 'jotai'
+import { filteredListingsAtom } from '@/store/listingsStore'
 
 const Page = () => {
-    const [category, setCategory] = useState(accomodation_categories[0].name);
-    const [listings] = useAtom(filteredListingsAtom);
-    const listingsGeo = useMemo(() => listingsDataGeo as ListingGeo, []);
+  const [category, setCategory] = useState(accomodation_categories[0].name)
+  const [listings] = useAtom(filteredListingsAtom)
+  const listingsGeo = useMemo(() => listingsDataGeo as ListingGeo, [])
 
-    const onDataChanged = (category: string) => {
-        setCategory(category);
-    };
+  const onDataChanged = useCallback((category: string) => {
+    setCategory(category)
+  }, [])
 
-    return (
-        <View style={defaultStyles.flex}>
-            <Stack.Screen
-                options={{
-                    header: () => (
-                        <ExploreHeader onCategoryChanged={onDataChanged} />
-                    ),
-                }}
-            />
-            {/* <Listings category={category} listings={listings} /> */}
-            <ListingsMap listings={listingsGeo} />
-            <ListingsBottomSheet listings={listings} category={category} />
-        </View>
-    );
-};
-export default Page;
+  return (
+    <View style={defaultStyles.flex}>
+      <Stack.Screen
+        options={{
+          header: () => <ExploreHeader onCategoryChanged={onDataChanged} />,
+        }}
+      />
+
+      <ListingsMap listings={listingsGeo} />
+      <ListingsBottomSheet listings={listings} category={category} />
+    </View>
+  )
+}
+export default Page
