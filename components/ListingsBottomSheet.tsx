7812ae967fc900880be826877b9d3579ebf0449e
waleery <walenda1997@gmail.com>
2024-08-27 17:42:57 +0200

    refactor(BottomSheet): ListingsBottomSheet

--- components/ListingsBottomSheet.tsx
+++ components/ListingsBottomSheet.tsx
@@ -1,12 +1,11 @@
 import { Ionicons } from '@expo/vector-icons'
 import BottomSheet from '@gorhom/bottom-sheet'
-import { useMemo, useRef, useState } from 'react'
+import { useCallback, useMemo, useRef, useState } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 
 import Listings from './Listings'
 
-import colors from '@/constants/Colors'
 import Colors from '@/constants/Colors'
 import { defaultStyles } from '@/constants/Styles'
 import { Listing } from '@/types/listing'
@@ -21,17 +20,17 @@ const ListingsBottomSheet = ({ listings, category }: Props) => {
   const snapPoitns = useMemo(() => ['10%', '100%'], [])
   const [refresh, setRefresh] = useState(0)
 
-  const showMap = () => {
+  const showMap = useCallback(() => {
     bottomSheetRef.current?.collapse()
     setRefresh(refresh + 1)
-  }
+  }, [refresh])
 
   return (
     <BottomSheet
       index={1}
       ref={bottomSheetRef}
       snapPoints={snapPoitns}
-      handleIndicatorStyle={{ backgroundColor: colors.grey }}
+      handleIndicatorStyle={{ backgroundColor: Colors.grey }}
       style={styles.sheetContainer}
     >
       <View style={defaultStyles.flex}>
@@ -55,7 +54,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   btn: {
-    backgroundColor: colors.dark,
+    backgroundColor: Colors.dark,
     padding: 16,
     height: 50,
     alignItems: 'center',
@@ -64,10 +63,10 @@ const styles = StyleSheet.create({
     gap: 8,
   },
   sheetContainer: {
-    backgroundColor: '#fff',
+    backgroundColor: Colors.white,
     borderRadius: 0,
     elevation: 4,
-    shadowColor: '#000',
+    shadowColor: Colors.black,
     shadowOpacity: 0.3,
     shadowRadius: 4,
     shadowOffset: {
