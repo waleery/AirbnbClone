d6f296a72294b4171e9d07cfc44016fa6543a811
waleery <walenda1997@gmail.com>
2024-08-30 23:57:34 +0200

    refactor(Modals): ModalHeaderText

--- components/ModalHeaderText.tsx
+++ components/ModalHeaderText.tsx
@@ -1,18 +1,21 @@
-import { useState } from 'react'
+/* eslint-disable react-native/no-inline-styles */
+import { useCallback, useState } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 
-import colors from '@/constants/Colors'
+import Colors from '@/constants/Colors'
 const ModalHeaderText = () => {
   const [active, setActive] = useState(0)
+
+  const handleSetActive = useCallback((index: number) => () => setActive(index), [])
   return (
     <View style={styles.headerContainer}>
-      <TouchableOpacity onPress={() => setActive(0)}>
+      <TouchableOpacity onPress={handleSetActive(0)}>
         <Text
           style={[
             styles.headerText,
             {
-              color: active === 0 ? '#000' : colors.grey,
+              color: active === 0 ? Colors.black : Colors.grey,
               textDecorationLine: active === 0 ? 'underline' : 'none',
             },
           ]}
@@ -20,12 +23,12 @@ const ModalHeaderText = () => {
           Stays
         </Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => setActive(1)}>
+      <TouchableOpacity onPress={handleSetActive(1)}>
         <Text
           style={[
             styles.headerText,
             {
-              color: active === 1 ? '#000' : colors.grey,
+              color: active === 1 ? Colors.black : Colors.grey,
               textDecorationLine: active === 1 ? 'underline' : 'none',
             },
           ]}
