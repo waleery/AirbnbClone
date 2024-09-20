7a551a716330bbb5c12fca1043b66e7c84cc973c
waleery <walenda1997@gmail.com>
2024-09-20 11:01:04 +0200

    feat(Profile): add file with setings name and icon

---
+++ assets/data/settings.ts
@@ -0,0 +1,34 @@
+export const settings = [
+  {
+    title: 'Personal information',
+    icon: 'person-circle-outline',
+  },
+  {
+    title: 'Payments and payouts',
+    icon: 'cash-outline',
+  },
+  {
+    title: 'Taxes',
+    icon: 'document-outline',
+  },
+  {
+    title: 'Login & security',
+    icon: 'shield-outline',
+  },
+  {
+    title: 'Accessibility',
+    icon: 'accessibility-outline',
+  },
+  {
+    title: 'Translation',
+    icon: 'language-outline',
+  },
+  {
+    title: 'Notifications',
+    icon: 'notifications-outline',
+  },
+  {
+    title: 'Privacy and sharing',
+    icon: 'lock-closed-outline',
+  },
+]
