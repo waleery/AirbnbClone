7850e94f4b38a96832a7bdd27463ec4ce5e84cbd
waleery <walenda1997@gmail.com>
2024-09-23 09:57:56 +0200

    fix: ensure proper icon type assignment in settings types

--- assets/data/settings.ts
+++ assets/data/settings.ts
@@ -1,34 +1,38 @@
+import { Ionicons } from '@expo/vector-icons'
+
+type IconNames = keyof typeof Ionicons.glyphMap
+
 export const settings = [
   {
     title: 'Personal information',
-    icon: 'person-circle-outline',
+    icon: 'person-circle-outline' as IconNames,
   },
   {
     title: 'Payments and payouts',
-    icon: 'cash-outline',
+    icon: 'cash-outline' as IconNames,
   },
   {
     title: 'Taxes',
-    icon: 'document-outline',
+    icon: 'document-outline' as IconNames,
   },
   {
     title: 'Login & security',
-    icon: 'shield-outline',
+    icon: 'shield-outline' as IconNames,
   },
   {
     title: 'Accessibility',
-    icon: 'accessibility-outline',
+    icon: 'accessibility-outline' as IconNames,
   },
   {
     title: 'Translation',
-    icon: 'language-outline',
+    icon: 'language-outline' as IconNames,
   },
   {
     title: 'Notifications',
-    icon: 'notifications-outline',
+    icon: 'notifications-outline' as IconNames,
   },
   {
     title: 'Privacy and sharing',
-    icon: 'lock-closed-outline',
+    icon: 'lock-closed-outline' as IconNames,
   },
 ]
