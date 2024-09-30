c4d4123b30445a5324fc32b4ab8ff5da05b3dc29
waleery <walenda1997@gmail.com>
2024-09-30 20:40:06 +0200

    feat(Filter): Define type of pleces img's

--- assets/data/places.ts
+++ assets/data/places.ts
@@ -1,33 +1,28 @@
-import world0 from '@/assets/data/world-0.png'
-import world1 from '@/assets/data/world-1.png'
-import world2 from '@/assets/data/world-2.png'
-import world3 from '@/assets/data/world-3.png'
-import world4 from '@/assets/data/world-4.png'
-import world5 from '@/assets/data/world-5.png'
+import { ImageSourcePropType } from 'react-native'
 
 export const places = [
   {
     title: "I'm flexible",
-    img: world0,
+    img: require('@/assets/data/world-0.png') as ImageSourcePropType,
   },
   {
     title: 'United States',
-    img: world1,
+    img: require('@/assets/data/world-1.png') as ImageSourcePropType,
   },
   {
     title: 'Italy',
-    img: world2,
+    img: require('@/assets/data/world-2.png') as ImageSourcePropType,
   },
   {
     title: 'Middle East',
-    img: world3,
+    img: require('@/assets/data/world-3.png') as ImageSourcePropType,
   },
   {
     title: 'Netherlands',
-    img: world4,
+    img: require('@/assets/data/world-4.png') as ImageSourcePropType,
   },
   {
     title: 'Southeast Asia',
-    img: world5,
+    img: require('@/assets/data/world-5.png') as ImageSourcePropType,
   },
 ]
