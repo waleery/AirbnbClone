0e51026218019300ce9a93cfa348c7b841eab9c2
waleery <walenda1997@gmail.com>
2024-10-01 16:19:08 +0200

    feat(Profile): Add file with support option names and icons

---
+++ assets/data/support.ts
@@ -0,0 +1,25 @@
+import { TOption } from '@/types/option'
+
+export const support: TOption[] = [
+  {
+    title: 'Visit the Help Center',
+    icon: 'help-circle-outline',
+  },
+  {
+    title: 'Get help with a sefty issue',
+    icon: 'shield-outline',
+  },
+  {
+    title: 'Report a neighborhood concer',
+    icon: 'headset-outline',
+  },
+  {
+    title: 'How Airbnb works',
+    icon: 'airbnb',
+    lib: 'FontAwesome5',
+  },
+  {
+    title: 'Give us feedback',
+    icon: 'pencil-outline',
+  },
+]
