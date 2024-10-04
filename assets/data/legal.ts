12cd25308cd6eddd135277ee824780a51ef99ccc
waleery <walenda1997@gmail.com>
2024-10-04 18:05:19 +0200

    feat(Profile): add file with legal names and icons

---
+++ assets/data/legal.ts
@@ -0,0 +1,14 @@
+export const legal = [
+  {
+    title: 'Terms of Service',
+    icon: 'book-outline',
+  },
+  {
+    title: 'Privacy Policy',
+    icon: 'book-outline',
+  },
+  {
+    title: 'Open source licences',
+    icon: 'book-outline',
+  },
+]
