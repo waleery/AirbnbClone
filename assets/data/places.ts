a77ec0e253f45bcbf293b97f89b8aa553aad4450
waleery <walenda1997@gmail.com>
2024-10-28 18:04:30 +0100

    refactor(App): move images with word's section to separate directory

--- assets/data/places.ts
+++ assets/data/places.ts
@@ -3,26 +3,26 @@ import { ImageSourcePropType } from 'react-native'
 export const places = [
   {
     title: "I'm flexible",
-    img: require('@/assets/data/world-0.png') as ImageSourcePropType,
+    img: require('@/assets/images/places/world-0.png') as ImageSourcePropType,
   },
   {
     title: 'United States',
-    img: require('@/assets/data/world-1.png') as ImageSourcePropType,
+    img: require('@/assets/images/places/world-1.png') as ImageSourcePropType,
   },
   {
     title: 'Italy',
-    img: require('@/assets/data/world-2.png') as ImageSourcePropType,
+    img: require('@/assets/images/places/world-2.png') as ImageSourcePropType,
   },
   {
     title: 'Middle East',
-    img: require('@/assets/data/world-3.png') as ImageSourcePropType,
+    img: require('@/assets/images/places/world-3.png') as ImageSourcePropType,
   },
   {
     title: 'Netherlands',
-    img: require('@/assets/data/world-4.png') as ImageSourcePropType,
+    img: require('@/assets/images/places/world-4.png') as ImageSourcePropType,
   },
   {
     title: 'Southeast Asia',
-    img: require('@/assets/data/world-5.png') as ImageSourcePropType,
+    img: require('@/assets/images/places/world-5.png') as ImageSourcePropType,
   },
 ]
