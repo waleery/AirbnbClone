2dbd5405a0be2deb85f7e8f0d43cc13edc554225
waleery <walenda1997@gmail.com>
2024-10-31 17:10:54 +0100

    feat(ProfilePage): Add SafeAreaView

--- app/(profilePage)/ProfilePage.tsx
+++ app/(profilePage)/ProfilePage.tsx
@@ -1,9 +1,20 @@
-import { View, Text } from 'react-native'
+import { Text, StyleSheet } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
+import Colors from '@/constants/Colors'
 export default function ProfilePage() {
   return (
-    <View>
+    <SafeAreaView edges={['top']} style={[styles.container]}>
       <Text>ProfilePage</Text>
-    </View>
+    </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.white,
+    flex: 1,
+    paddingTop:50,
+    paddingHorizontal: 16,
+  },
+})
