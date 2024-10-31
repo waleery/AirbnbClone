506fe23fd86aedda0191b897e7de2ffeec25dea6
waleery <walenda1997@gmail.com>
2024-10-31 16:53:00 +0100

    feat(ProfilePage): Add stack with ProfilePage

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -106,6 +106,7 @@ function RootLayoutNav() {
         }}
       />
       <Stack.Screen name="listing/[id]" options={{ headerTitle: '', headerTransparent: true }} />
+      <Stack.Screen name="(profilePage)/ProdilePage" />
       <Stack.Screen
         name="(modals)/booking"
         options={{
