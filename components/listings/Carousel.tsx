8effb35e103c61c43a68631046c8b9d616094918
waleery <walenda1997@gmail.com>
2025-01-12 10:08:22 +0100

    feat(listing): add example images to listing carousel

--- components/listings/Carousel.tsx
+++ components/listings/Carousel.tsx
@@ -1,5 +1,5 @@
 import { MaterialCommunityIcons } from '@expo/vector-icons'
-import React, { useCallback, useRef, useState } from 'react'
+import React, { useCallback, useMemo, useRef, useState } from 'react'
 import {
   View,
   FlatList,
@@ -21,11 +21,20 @@ type CarouselProps = {
   onPress?: () => void
 }
 
+const additionalImages = [
+  require('@/assets/images/room_example.jpg'),
+  require('@/assets/images/room_example_2.jpg'),
+  require('@/assets/images/room_example_3.jpg')
+];
+
 const Carousel: React.FC<CarouselProps> = ({ items, onPress = () => {} }) => {
   const [currentIndex, setCurrentIndex] = useState(0)
   const [imageError, setImageError] = useState(false)
 
   const flatListRef = useRef<FlatList<string>>(null)
+
+  const allImages = useMemo(() => [...items, ...additionalImages], [items]);
+
 
   const handleImageError = useCallback(() => setImageError(true), [])
 
@@ -45,24 +54,30 @@ const Carousel: React.FC<CarouselProps> = ({ items, onPress = () => {} }) => {
 
   const keyExtractor = useCallback((_: string, index: number) => index.toString(), [])
   const renderItem = useCallback(
-    ({ item }: { item: string }) => {
+    ({ item }: { item: string | number }) => {
       if (imageError) {
         return (
           <View style={styles.imagePlaceholder}>
             <MaterialCommunityIcons name="image-off-outline" size={50} color={Colors.primary} />
             <Text>Image failed to load</Text>
           </View>
-        )
+        );
       } else {
         return (
           <Pressable onPress={handleOnPress}>
-            <Image source={{ uri: item }} style={styles.image} onError={handleImageError} />
+            {/* Sprawdź, czy `item` jest lokalnym obrazem */}
+            <Image
+              source={typeof item === 'string' ? { uri: item } : item}
+              style={styles.image}
+              onError={handleImageError}
+            />
           </Pressable>
-        )
+        );
       }
     },
     [handleImageError, imageError, handleOnPress]
-  )
+  );
+  
   const handleDotPress = useCallback(
     (index: number) => () => {
       setCurrentIndex(index)
@@ -76,7 +91,7 @@ const Carousel: React.FC<CarouselProps> = ({ items, onPress = () => {} }) => {
     <View style={styles.container}>
       <FlatList
         ref={flatListRef}
-        data={items}
+        data={allImages}
         horizontal
         showsHorizontalScrollIndicator={false}
         pagingEnabled
@@ -87,7 +102,7 @@ const Carousel: React.FC<CarouselProps> = ({ items, onPress = () => {} }) => {
       />
       {items.length > 1 && (
         <View style={styles.dotsContainer}>
-          {items.map((_, index) => (
+          {allImages.map((_, index) => (
             <TouchableOpacity
               key={index}
               style={[styles.dot, currentIndex === index && styles.activeDot]}
