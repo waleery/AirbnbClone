1b52f189035334698f56156834594cf56f4b180e
waleery <walenda1997@gmail.com>
2025-01-24 18:24:25 +0100

    feat(Layout): integrate EarnBottomSheet component into the main layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,62 +2,68 @@ import { FontAwesome5, Ionicons, MaterialCommunityIcons } from '@expo/vector-ico
 import { Tabs } from 'expo-router'
 
 import { InboxHeader, WishlistHeader } from '@/components'
+import { EarnBottomSheet } from '@/components/profile/EarnBottomSheet'
 import Colors from '@/constants/Colors'
 
 const Layout = () => {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors.primary,
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          tabBarLabel: 'Explore',
-          tabBarIcon: ({ color, size }) => <Ionicons name="search" color={color} size={size} />,
+    <>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: Colors.primary,
         }}
-      />
-      <Tabs.Screen
-        name="Wishlists"
-        options={{
-          tabBarLabel: 'Wishlists',
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="heart-outline" color={color} size={size} />
-          ),
-          header: () => <WishlistHeader />,
-          unmountOnBlur: true,
-        }}
-      />
-      <Tabs.Screen
-        name="Trips"
-        options={{
-          tabBarLabel: 'Trips',
-          tabBarIcon: ({ color, size }) => <FontAwesome5 name="airbnb" color={color} size={size} />,
-          headerShown: false,
-        }}
-      />
-      <Tabs.Screen
-        name="Inbox"
-        options={{
-          header: () => <InboxHeader />,
-          tabBarLabel: 'Inbox',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="message-outline" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="Profile"
-        options={{
-          tabBarLabel: 'Profile',
-          headerShown: false,
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="person-circle-outline" color={color} size={size} />
-          ),
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            tabBarLabel: 'Explore',
+            tabBarIcon: ({ color, size }) => <Ionicons name="search" color={color} size={size} />,
+          }}
+        />
+        <Tabs.Screen
+          name="Wishlists"
+          options={{
+            tabBarLabel: 'Wishlists',
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="heart-outline" color={color} size={size} />
+            ),
+            header: () => <WishlistHeader />,
+            unmountOnBlur: true,
+          }}
+        />
+        <Tabs.Screen
+          name="Trips"
+          options={{
+            tabBarLabel: 'Trips',
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome5 name="airbnb" color={color} size={size} />
+            ),
+            headerShown: false,
+          }}
+        />
+        <Tabs.Screen
+          name="Inbox"
+          options={{
+            header: () => <InboxHeader />,
+            tabBarLabel: 'Inbox',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="message-outline" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="Profile"
+          options={{
+            tabBarLabel: 'Profile',
+            headerShown: false,
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="person-circle-outline" color={color} size={size} />
+            ),
+          }}
+        />
+      </Tabs>
+      <EarnBottomSheet />
+    </>
   )
 }
 export default Layout
