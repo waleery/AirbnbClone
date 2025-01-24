b30504d5f09bacc7a47c1802fea133564efe836e
waleery <walenda1997@gmail.com>
2025-01-24 17:53:49 +0100

    feat(EarnBottomSheet): add initial implementation of EarnBottomSheet component

---
+++ components/profile/EarnBottomSheet.tsx
@@ -0,0 +1,8 @@
+import BottomSheet from '@gorhom/bottom-sheet'
+import { StyleSheet } from 'react-native'
+
+export const EarnBottomSheet = () => {
+  return <BottomSheet></BottomSheet>
+}
+
+const styles = StyleSheet.create({})
