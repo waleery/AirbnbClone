fcc71fc7204bf108c6f810e957915931f83c3764
waleery <walenda1997@gmail.com>
2025-01-24 18:19:30 +0100

    feat(EarnBottomSheet): add functions to expand and close

--- components/profile/EarnBottomSheet.tsx
+++ components/profile/EarnBottomSheet.tsx
@@ -1,8 +1,39 @@
 import BottomSheet from '@gorhom/bottom-sheet'
-import { StyleSheet } from 'react-native'
+import { createRef, useMemo } from 'react'
+import { View, StyleSheet } from 'react-native'
 
-export const EarnBottomSheet = () => {
-  return <BottomSheet></BottomSheet>
+import Colors from '@/constants/Colors'
+import { defaultStyles } from '@/constants/Styles'
+
+export const earnBottomSheetRef = createRef<BottomSheet>()
+
+// METHODS
+export const handlePresentEarnBottomSheet = () => {
+  earnBottomSheetRef.current?.expand?.()
 }
 
-const styles = StyleSheet.create({})
+export const handleCloseEarnBottomSheet = () => {
+  earnBottomSheetRef.current?.close?.()
+}
+
+export const EarnBottomSheet = () => {
+  const snapPoints = useMemo(() => ['100%', '100%'], [])
+
+  return (
+    <BottomSheet
+      ref={earnBottomSheetRef}
+      snapPoints={snapPoints}
+      index={-1}
+      style={[styles.sheetContainer]}
+      enablePanDownToClose={true}
+    >
+      <View style={defaultStyles.flex}></View>
+    </BottomSheet>
+  )
+}
+
+const styles = StyleSheet.create({
+  sheetContainer: {
+    backgroundColor: Colors.white,
+  },
+})
