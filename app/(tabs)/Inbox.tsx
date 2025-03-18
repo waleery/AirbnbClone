5029b2a1e24293b0ebe17b49f37bf6f2465a36ef
waleery <walenda1997@gmail.com>
2025-03-18 16:01:26 +0100

    feat(Inbox): add UnauthorizedInbox component for users not signed in

--- app/(tabs)/Inbox.tsx
+++ app/(tabs)/Inbox.tsx
@@ -1,10 +1,11 @@
 import { useAuth } from '@clerk/clerk-expo'
 
 import AuthorizedInbox from "@/components/inbox/AuthorizedInbox";
+import UnauthorizedInbox from '@/components/inbox/UnauthorizedInbox';
 
 const Page = () => {
 	const { isSignedIn } = useAuth()
 
-	return <> {isSignedIn ? <AuthorizedInbox /> : null}</>
+	return <> {isSignedIn ? <AuthorizedInbox /> : <UnauthorizedInbox/> }</>
 };
 export default Page;
