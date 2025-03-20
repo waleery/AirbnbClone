a157798cf155b63f6ae5937c2de2798ac4951ec4
waleery <walenda1997@gmail.com>
2025-03-20 23:51:54 +0100

    feat(Inbox): wrap UnauthorizedInbox component in SafeAreaView for better layout

--- components/inbox/UnauthorizedInbox.tsx
+++ components/inbox/UnauthorizedInbox.tsx
@@ -1,11 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { defaultStyles } from '@/constants/Styles'
 
 const UnauthorizedInbox = () => {
   return (
-    <View>
+    <SafeAreaView edges={['top']} style={[defaultStyles.pX2, defaultStyles.safeArea]}>
       <Text>UnauthorizedInbox</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 
