d505692d59f324caf547f1f16acb1968095881c2
waleery <walenda1997@gmail.com>
2025-05-08 23:25:58 +0200

    fix(RootLayout): simplify error handling in useEffect

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -55,9 +55,7 @@ export default function RootLayout() {
   })
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-  useEffect(() => {
-    if (error) throw error
-  }, [error])
+  if (error) throw error
 
   useEffect(() => {
     if (loaded) {
