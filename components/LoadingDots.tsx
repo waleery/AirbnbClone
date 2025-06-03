81388ad45ac5e3a7a25700d2d86039535da5cf0d
waleery <walenda1997@gmail.com>
2025-06-03 17:30:26 +0200

    fix(LoadingDots): specify type for dots state as Animated.Value[]

--- components/LoadingDots.tsx
+++ components/LoadingDots.tsx
@@ -8,7 +8,7 @@ type LoadingDotsProps = {
 }
 
 export const LoadingDots = ({ numberOfDots = 3 }: LoadingDotsProps) => {
-  const dots = useRef([...Array(numberOfDots)].map(() => new Animated.Value(0))).current
+  const dots = useRef<Animated.Value[]>([...Array(numberOfDots)].map(() => new Animated.Value(0))).current
 
   useEffect(() => {
     const sequence = (dot: Animated.Value) =>
