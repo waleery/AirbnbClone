b2ea8efef96adf3c5f4bf0688657bd556fde4242
waleery <walenda1997@gmail.com>
2025-08-06 18:11:01 +0200

    feat: add StartPage component with basic layout

---
+++ components/listings/StartPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+import { defaultStyles } from '@/constants'
+
+export const StartPage = () => {
+  return (
+    <View style={defaultStyles.flex}>
+      <Text>StartPage</Text>
+    </View>
+  )
+}
