3abdfdd67d1f369e6f1ab8e394521e7c7d629867
waleery <walenda1997@gmail.com>
2025-08-26 22:12:21 +0200

    refactor: move button to show map to main page

--- app/(tabs)/Index.tsx
+++ app/(tabs)/Index.tsx
@@ -1,34 +1,68 @@
-import { Stack } from 'expo-router'
-import { useAtomValue } from 'jotai'
-import { useCallback, useMemo, useState } from 'react'
-import { View } from 'react-native'
+import { Stack } from "expo-router";
+import { useAtomValue } from "jotai";
+import { useCallback, useMemo, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-import listingsDataGeo from '@/assets/data/json/airbnb-listings.geo.json'
-import { ExploreHeader, ListingsBottomSheet, ListingsMap } from '@/components'
-import { defaultStyles, accommodation_categories } from '@/constants'
-import { filteredListingsAtom } from '@/store'
-import { ListingGeo } from '@/types'
+import listingsDataGeo from "@/assets/data/json/airbnb-listings.geo.json";
+import { bottomSheetRef, ExploreHeader, ListingsBottomSheet, ListingsMap } from "@/components";
+import { defaultStyles, accommodation_categories } from "@/constants";
+import { filteredListingsAtom } from "@/store";
+import { ListingGeo } from "@/types";
+import { Pressable } from "react-native-gesture-handler";
+import { Ionicons } from "@expo/vector-icons";
+import Colors from "@/constants/Colors";
 
 const Page = () => {
-  const [category, setCategory] = useState<string>(accommodation_categories[0].name)
-  const listings = useAtomValue(filteredListingsAtom)
-  const listingsGeo = useMemo(() => listingsDataGeo as ListingGeo, [])
+	const [category, setCategory] = useState<string>(
+		accommodation_categories[0].name,
+	);
+	const listings = useAtomValue(filteredListingsAtom);
+	const listingsGeo = useMemo(() => listingsDataGeo as ListingGeo, []);
 
-  const onDataChanged = useCallback((category: string) => {
-    setCategory(category)
-  }, [])
+	const onDataChanged = useCallback((category: string) => {
+		setCategory(category);
+	}, []);
 
-  return (
-    <View style={defaultStyles.flex}>
-      <Stack.Screen
-        options={{
-          header: () => <ExploreHeader onCategoryChanged={onDataChanged} />,
-        }}
-      />
+	const showMap = useCallback(() => {
+		bottomSheetRef.current?.collapse();
+		// setRefresh(refresh + 1);
+	}, []);
 
-      <ListingsMap listings={listingsGeo} />
-      <ListingsBottomSheet listings={listings} category={category} />
-    </View>
-  )
-}
-export default Page
+	return (
+		<View style={defaultStyles.flex}>
+			<Stack.Screen
+				options={{
+					header: () => <ExploreHeader onCategoryChanged={onDataChanged} />,
+				}}
+			/>
+
+			<ListingsMap listings={listingsGeo} />
+			<ListingsBottomSheet listings={listings} category={category} />
+			<View style={styles.absoluteBtn}>
+				<Pressable onPress={showMap} style={styles.btn}>
+					<Text style={[defaultStyles.white, defaultStyles.boldText]}>Map</Text>
+					<Ionicons name="map" size={17} color={"#fff"} />
+				</Pressable>
+			</View>
+		</View>
+	);
+};
+export default Page;
+
+const styles = StyleSheet.create({
+	absoluteBtn: {
+		position: "absolute",
+		bottom: 25,
+		width: "100%",
+		alignItems: "center",
+	},
+	btn: {
+		backgroundColor: Colors.dark,
+		padding: 13,
+		paddingHorizontal: 20,
+		alignItems: "center",
+		flexDirection: "row",
+		borderRadius: 30,
+		gap: 8,
+	},
+});
--- components/listings/ListingsBottomSheet.tsx
+++ components/listings/ListingsBottomSheet.tsx
@@ -1,80 +1,61 @@
-import { Ionicons } from '@expo/vector-icons'
-import BottomSheet from '@gorhom/bottom-sheet'
-import { useCallback, useMemo, useRef, useState } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import { Pressable } from 'react-native-gesture-handler'
+import { Ionicons } from "@expo/vector-icons";
+import BottomSheet from "@gorhom/bottom-sheet";
+import { createRef, useCallback, useMemo, useRef, useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { Pressable } from "react-native-gesture-handler";
 
-import { Listings } from './Listings'
+import { Listings } from "./Listings";
 
-import { defaultStyles } from '@/constants'
-import Colors from '@/constants/Colors'
-import { Listing } from '@/types'
+import { defaultStyles } from "@/constants";
+import Colors from "@/constants/Colors";
+import { Listing } from "@/types";
 
 interface Props {
-  listings: Listing[]
-  category: string
+	listings: Listing[];
+	category: string;
 }
+
+export const bottomSheetRef = createRef<BottomSheet>();
 
 export const ListingsBottomSheet = ({ listings, category }: Props) => {
-  const bottomSheetRef = useRef<BottomSheet>(null)
-  const snapPoitns = useMemo<string[]>(() => ['10%', '100%'], [])
-  const [refresh, setRefresh] = useState<number>(0)
+	const snapPoitns = useMemo<string[]>(() => ["10%", "100%"], []);
+	const [refresh, setRefresh] = useState<number>(0);
 
-  const showMap = useCallback(() => {
-    bottomSheetRef.current?.collapse()
-    setRefresh(refresh + 1)
-  }, [refresh])
-
-  return (
-    <BottomSheet
-      index={1}
-      ref={bottomSheetRef}
-      snapPoints={snapPoitns}
-      topInset={-50}
-      handleIndicatorStyle={{ backgroundColor: Colors.grey }}
-      style={styles.sheetContainer}
-    >
-      <View style={defaultStyles.flex}>
-        <Listings listings={listings} category={category} refresh={refresh} />
-        <View style={styles.absoluteBtn}>
-          <Pressable onPress={showMap} style={styles.btn}>
-            <Text style={[defaultStyles.white, defaultStyles.boldText]}>Map</Text>
-            <Ionicons name="map" size={17} color={'#fff'} />
-          </Pressable>
-        </View>
-      </View>
-    </BottomSheet>
-  )
-}
+	return (
+		<BottomSheet
+			index={1}
+			ref={bottomSheetRef}
+			snapPoints={snapPoitns}
+			topInset={-50}
+			handleIndicatorStyle={{ backgroundColor: Colors.grey }}
+			style={styles.sheetContainer}
+		>
+			<View style={defaultStyles.flex}>
+				<Listings listings={listings} category={category} refresh={refresh} />
+			</View>
+		</BottomSheet>
+	);
+};
 
 const styles = StyleSheet.create({
-  absoluteBtn: {
-    position: 'absolute',
-    bottom: 25,
-    width: '100%',
-    alignItems: 'center',
-  },
-  btn: {
-    backgroundColor: Colors.dark,
-    padding: 13,
-    paddingHorizontal: 20,
-    alignItems: 'center',
-    flexDirection: 'row',
-    borderRadius: 30,
-    gap: 8,
-  },
-  sheetContainer: {
-    backgroundColor: Colors.white,
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,
-    borderRadius: 0,
-    elevation: 4,
-    shadowColor: Colors.black,
-    shadowOpacity: 0.3,
-    shadowRadius: 4,
-    shadowOffset: {
-      width: 1,
-      height: 1,
-    },
-  },
-})
+	absoluteBtn: {
+		position: "absolute",
+		bottom: 25,
+		width: "100%",
+		alignItems: "center",
+	},
+	sheetContainer: {
+		backgroundColor: Colors.white,
+		borderTopLeftRadius: 20,
+		borderTopRightRadius: 20,
+		borderRadius: 0,
+		elevation: 4,
+		shadowColor: Colors.black,
+		shadowOpacity: 0.3,
+		shadowRadius: 4,
+		shadowOffset: {
+			width: 1,
+			height: 1,
+		},
+	},
+});
