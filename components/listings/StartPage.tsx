b82fd6e978b37ca80f7fcfff426e5a134064acfc
waleery <walenda1997@gmail.com>
2025-09-07 23:12:32 +0200

    feat(StartPage): Add horizontal list header

--- components/listings/StartPage.tsx
+++ components/listings/StartPage.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 import { defaultStyles } from '@/constants'
+import Colors from '@/constants/Colors'
 
 export const StartPage = () => {
   return (
     <View style={defaultStyles.flex}>
-      <Text>StartPage</Text>
+      <Text style={[styles.header, defaultStyles.boldText]}>Popular homes in Warsaw</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  header: {
+    marginTop: 30,
+    fontSize: 20,
+  },
+})
